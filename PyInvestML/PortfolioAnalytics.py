9fc3a8cba3ad20b78bee11967b70b2a3c6573125
eddyzhang2018 <60183020+eddyzhang2018@users.noreply.github.com>
2021-03-04 15:42:21 +1100

    Update PortfolioAnalytics.py

--- PyInvestML/PortfolioAnalytics.py
+++ PyInvestML/PortfolioAnalytics.py
@@ -1,1 +1,13 @@
+# To creat function allowing portfolio analytics
 
+import pandas as pd
+
+def drawdown(return_series: pd.Series):
+    
+    wealth_index = 10000*(1+return_series).cumprod()
+    previous_peaks = wealth_index.cummax()
+    drawdowns = (wealth_index - previous_peaks)/previous_peaks
+    
+    return pd.DataFrame({"10K Growth": wealth_index,
+                         "Previous Peak": previous_peaks,
+                         "Drawdown": drawdowns})
