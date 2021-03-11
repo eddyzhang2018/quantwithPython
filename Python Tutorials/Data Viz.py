077244bcb6e5e874c6d0d686c0e3307c4ceefbf1
eddyzhang2018 <60183020+eddyzhang2018@users.noreply.github.com>
2021-03-12 02:01:22 +1100

    Update Data Viz.py

--- Python Tutorials/Data Viz.py
+++ Python Tutorials/Data Viz.py
@@ -1,3 +1,12 @@
 # Data Vis with Python
 
 
+# Libraray
+
+%matplotlib inline
+
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches # needed for waffle Charts
+
+mpl.style.use('ggplot') # optional: for ggplot-like style
